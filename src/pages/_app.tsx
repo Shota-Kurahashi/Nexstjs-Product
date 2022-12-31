d8dff2d078dbbf96fb7a728be1f48048225564c3
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-31 19:30:49 +0900

    axios and TanStacQuery

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/tailwind.css";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import queryClient from "../libs/queryClient";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const [client] = useState(() => queryClient);
+
+  return (
+    <QueryClientProvider client={client}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
